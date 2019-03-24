38ad1d753e733bac401c9cef380a6bb0f298f425
DanielaFerreira <danielaferreira1133@gmail.com>
2019-03-23 21:15:45 -0400

    Inicio sessao marketing na homepage, correcao logo, e do visualizar senha

--- target/adopets-1.0-SNAPSHOT/index.js
+++ target/adopets-1.0-SNAPSHOT/index.js
@@ -4,22 +4,22 @@ $(function () {
 
 $(document).ready(function () {
     $("#icon-click").click(function () {
-        $("#icon").toggleClass('fa fa-eye-slash');
         var input = $("#pass");
         if (input.attr("type") === "password") {
             input.attr("type", "text");
+            $("#icon").toggleClass('fa fa-eye');
         } else {
             input.attr("type", "password");
+            $("#icon").toggleClass('fa fa-eye-slash');
         }
     });
 });
 
 function capLock(e) {
     kc = e.keyCode ? e.keyCode : e.which;
-    sk = e.shiftKey ? e.shiftKey : ((kc == 16) ? true : false);
+    sk = e.shiftKey ? e.shiftKey : ((kc === 16) ? true : false);
     if (((kc >= 65 && kc <= 90) && !sk) || ((kc >= 97 && kc <= 122) && sk))
         document.getElementById('divMayus').style.visibility = 'visible';
     else
         document.getElementById('divMayus').style.visibility = 'hidden';
 }
-
